ff27867257b9ffba25b7b31307645f693663f015
Robson Rodrigues <bobson278@gmail.com>
2018-08-26 01:50:40 -0300

    CORS habilitado

--- src/app.js
+++ src/app.js
@@ -24,7 +24,17 @@ app.use(bodyParser.json({//define um limite para a requisão em JSON
     limit: '5mb'
 }))
 
-app.use(bodyParser.urlencoded({ extended:false }))
+app.use(bodyParser.urlencoded({ 
+    extended:false 
+}))
+
+// Habilita o CORS
+app.use(function (req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 
 app.use('/', index)
 app.use('/products', products)
