116b09eddc9a60811c20843f6474ba32bad47216
ROBSON RODRIGUES DA SILVA <bobson278@gmail.com>
2018-09-03 14:58:50 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 
 module.exports = {
-    connectionString: 'mongodb://bobson360:bobson360@ds018568.mlab.com:18568/nodestr',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodebob;AccountKey=wRF1z3LsBlCjfNZc8BrcB6rGUbVaL5uk2npuHnaCIAIO05CkWDR1q0XaYboV1AFwnYVQ5Rhw2USazlkG0QsFxA==;EndpointSuffix=core.windows.net'
+    connectionString: '',
+    sendgridKey:'' ,
+    containerConnectionString: ''
     
 }
